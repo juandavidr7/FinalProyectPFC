334c7bb5d0146ee2aa16f9f3e377015ed078e762
Jhon Murillo <jhon.antony.murillo@correounivalle.edu.co>
2025-05-29 20:13:08 -0500

    Implementacion de la version paralela mejorada

--- src/main/scala/ReconstCadenasPar/package.scala
+++ src/main/scala/ReconstCadenasPar/package.scala
@@ -13,9 +13,33 @@ package object ReconstCadenasPar {
 
   // Versión paralela mejorada
   def reconstruirCadenaMejoradoPar(umbral: Int)(n: Int, o: Oraculo): Seq[Char] = {
-    // Usa la propiedad de que si s = s1 ++ s2 entonces s1 y s2 también son subsecuencias de s
-    // Usa paralelismo de tareas y/o datos
-    ???
+
+    @annotation.tailrec
+    def construirCandidatos(k: Int, scPrev: Seq[Seq[Char]]): Seq[Char] = {
+      // Decidir si paralelizar o no
+      val fuente = {
+        if (scPrev.size > umbral) scPrev.par
+        else scPrev
+      }
+
+      // Generación y filtrado con expresión for
+      // Si "fuente" es ParSeq, correrá en paralelo.
+      val scK: Seq[Seq[Char]] = (for {
+        prefijo <- fuente
+        ch <- Oraculo.alfabeto
+        candidato = prefijo :+ ch
+        if o(candidato)
+      } yield candidato).toList
+
+      // Buscar solución de longitud n usando match
+      scK.filter(_.length == n) match {
+        case sol :: _ => sol
+        case Nil => construirCandidatos(k + 1, scK)
+      }
+    }
+
+    // Punto de entrada
+    construirCandidatos(1, Seq(Seq.empty[Char]))
   }
 
   // Versión paralela turbo
